67795fc9033c509a447198cd9e600ffe3f1891bf
Kshitij-Taywade <shitutaywade345@gmail.com>
2026-04-06 15:01:17 +0530

    upadted something

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,18 @@
 import app from "./src/app.js"
 import connectDB from "./src/config/database.js";
 import config from "./src/config/config.js"
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+dotenv.config();
 
 
 connectDB();
 
 
 
-app.listen(process.env.PORT, () => {
+const PORT = process.env.PORT || 5000;
+
+
+
+app.listen(PORT, () => {
     console.log(`Server has been started on port ${PORT}`);
 });
