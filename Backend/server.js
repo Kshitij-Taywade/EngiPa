4c5ecc3d4c109d2330fca27357c24e1618beacdd
Kshitij-Taywade <shitutaywade345@gmail.com>
2026-04-06 15:06:13 +0530

    upadted server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,20 @@
-import app from "./src/app.js"
+console.log("1️⃣ Server file started");
+
+import app from "./src/app.js";
+console.log("2️⃣ App imported");
+
 import connectDB from "./src/config/database.js";
-import config from "./src/config/config.js"
+console.log("3️⃣ DB file imported");
+
 import dotenv from "dotenv";
 dotenv.config();
-
+console.log("4️⃣ Env loaded");
 
 connectDB();
-
-
+console.log("5️⃣ DB connection called");
 
 const PORT = process.env.PORT || 5000;
 
-
-
 app.listen(PORT, () => {
-    console.log(`Server has been started on port ${PORT}`);
+    console.log(`6️⃣ Server started on port ${PORT}`);
 });
