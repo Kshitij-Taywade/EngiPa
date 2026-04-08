d142d47c5b324b456e0b587d8c9e880b7b44d0a6
Kshitij-Taywade <shitutaywade345@gmail.com>
2026-04-08 16:56:35 +0530

    addesd some file

---
+++ backend2/src/models/admin.model.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose"
+
+const adminSchema = new mongoose.Schema({
+
+    fullName: {
+        type: String,
+        required: true,
+    },
+    clg_ID: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 10
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    mobile: {
+        type: String,
+        unique: true,
+        required: true,
+
+    },
+    department: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        type: String,
+        default: "https://cdn-icons-png.flaticon.com/128/3177/3177440.png"
+    },
+    role: {
+        type: String,
+        default: "admin"
+
+    },
+
+
+}, { timestamps: true });
+
+
+const adminModel = mongoose.model("admin", adminSchema);
+
+
+export default adminModel;
---
+++ backend2/src/models/paper.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const paperSchema = new mongoose.Schema({
+    file: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    subject: {
+        type: String,
+        required: true
+    },
+    semester: {
+        type: Number,
+        required: true
+    }, // number
+    year: {
+        type: Number,
+        required: true
+    }, // number
+    paper_type: {
+        type: String,
+        required: true
+    }, // string
+    department: {
+        type: String,
+        required: true
+    } // string
+}, { timestamps: true });
+
+const Paper = mongoose.model("Paper", paperSchema);
+export default Paper;
