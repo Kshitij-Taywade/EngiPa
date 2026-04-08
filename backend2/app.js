9b5e4009d097a756fe555608c60274256266b360
Kshitij-Taywade <shitutaywade345@gmail.com>
2026-04-08 18:10:09 +0530

    add cors  file

--- backend2/app.js
+++ backend2/app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./src/db/db.js"
+import cors from "cors"
 
 dotenv.config();
-
 connectDB();
-
 const app = express();
+
+app.use(cors());
+
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
