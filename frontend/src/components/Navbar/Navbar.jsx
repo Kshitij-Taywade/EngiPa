c706de24a0f74e357a2d0a96856f07dd0c7b56c6
Kshitij-Taywade <shitutaywade345@gmail.com>
2026-04-09 16:12:56 +0530

    added menu

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {useState} from 'react'
+import { GiHamburgerMenu } from "react-icons/gi";
 
 import logo from './EngiPa.png';
 
+
+
 const Navbar = () => {
+
+   const [isOpen, setIsOpen] = useState(false);
+
+  const menuOpnes = () => {
+    setIsOpen((prev) => !prev);
+  };
   return (
+     <>
     <div className="Navbar-parent">
       <div className="logo-div">
        <img src={logo} alt="EngiPa-logo"/>
@@ -16,7 +27,17 @@ const Navbar = () => {
       <Link to="/Contact-us" className="nav-links">Contact Us</Link>
 
       </div>
+      <button className="menu-icon" onClick={menuOpnes}><GiHamburgerMenu /></button>
+      
     </div>
+
+    <div className={`links-div2 ${isOpen ? "open" : ""}`}>
+  <Link to="/"  className="nav-links">Home</Link>
+      <Link to="/About-us" className="nav-links">About Us</Link>
+      <Link to="/Contact-us" className="nav-links">Contact Us</Link>
+
+    </div>
+    </>
   )
 }
 
